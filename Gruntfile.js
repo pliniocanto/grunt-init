753aa15fd534ee16e7f97bb20d8ea1aba7866cd8
Plínio Canto <pliniocanto@gmail.com>
2017-05-10 22:36:29 -0300

    grunt app init

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        /* Copia os arquivos para o diretório 'dist' */
+        copy: {
+            public: {
+                expand: true,
+                cwd: 'public',
+                src: '**',
+                dest: 'dist'
+            }
+        },
+
+        clean: {
+            dist: {
+                src: 'dist'
+            }
+        },
+
+        useminPrepare: {
+            html: 'dist/**/*.html'
+        },
+
+        usemin: {
+            html: 'dist/**/*.html'
+        }
+    });
+
+
+    //registrando task para minificação
+
+    grunt.registerTask('dist', ['clean', 'copy']);
+
+    grunt.registerTask('minifica', ['useminPrepare',
+        'concat', 'uglify', 'cssmin', 'usemin'
+    ]);
+
+    // registrando tasks
+    grunt.registerTask('default', ['dist', 'minifica']);
+
+    // carregando tasks
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+
+
+
+
+```
+npm init 
+sudo npm install grunt-cli -g
+npm install grunt --save-dev
+npm install grunt-contrib-copy --save-dev
+npm install grunt-contrib-clean --save-dev
+
+npm install grunt-contrib-concat --save-dev
+npm install grunt-contrib-uglify --save-dev
+npm install grunt-contrib-cssmin --save-dev
+npm install grunt-usemin --save-dev
+
+```
+
+
+Gruntfile.js init
+
+```javascript
+module.exports = function(grunt) {
+   grunt.initConfig({
+        copy: {
+              public: {
+                   cwd: 'public', 
+                   src: ['**'], 
+                   dest: 'dist', 
+                   expand: true
+              }
+         }, 
+         clean: {
+              dist: {
+                  src: ['dist']
+              }
+         }
+  });
+
+  grunt.registerTask('default', ['dist']);
+  grunt.registerTask('dist', ['clean', 'copy']);
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+};
+```
